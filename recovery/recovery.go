87009d8dd6c093a723d6d7f670953e29f8a7fd3c
Mikael Ståldal <mikael@staldal.nu>
2026-04-04 15:26:01 +0200

    The code

---
+++ recovery/recovery.go
@@ -0,0 +1,21 @@
+package recovery
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic: %v", err)
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusInternalServerError)
+				_ = json.NewEncoder(w).Encode(map[string]string{"error": "internal server error"})
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
