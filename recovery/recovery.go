b51ec7e4a5ee5071b9a27c158fd6f62ddcaadae4
Mikael Ståldal <mikael@staldal.nu>
2026-06-06 14:17:19 +0200

    Improve panic handling

--- recovery/recovery.go
+++ recovery/recovery.go
@@ -4,13 +4,17 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"runtime/debug"
 )
 
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
-				log.Printf("panic: %v", err)
+				if err == http.ErrAbortHandler {
+					panic(err)
+				}
+				log.Printf("panic: %v\n%s", err, debug.Stack())
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusInternalServerError)
 				_ = json.NewEncoder(w).Encode(map[string]string{"error": "internal server error"})
